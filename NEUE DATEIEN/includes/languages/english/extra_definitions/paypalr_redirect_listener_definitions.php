4635c4e6d1cd8d6ec9eef10e2e22275c0bf3a482
Jörg Schönberger <office@webchills.at>
2025-11-14 14:18:46 +0100

    missing language definitions added

--- NEUE DATEIEN/includes/languages/english/extra_definitions/paypalr_redirect_listener_definitions.php
+++ NEUE DATEIEN/includes/languages/english/extra_definitions/paypalr_redirect_listener_definitions.php
@@ -1,5 +1,7 @@
 <?php
 define('MODULE_PAYMENT_PAYPALR_REDIRECT_LISTENER_TRY_AGAIN', 'Please try again, select an alternate payment method or contact us for assistance.');
 define('EMAIL_TEXT_ORDER_CUSTOMER_NEUTRAL', 'Dear '); 
+define('EMAIL_TEXT_ORDER_CUSTOMER_GENDER_MALE', 'Dear Mr. ');
+define('EMAIL_TEXT_ORDER_CUSTOMER_GENDER_FEMALE', 'Dear Mrs. ');
 define('OSH_EMAIL_TEXT_ORDER_CUSTOMER_NEUTRAL', 'Dear '); 
 define('OSH_EMAIL_TEXT_UPDATEINFO', 'We inform you about your order status at '); 
--- NEUE DATEIEN/includes/languages/german/extra_definitions/paypalr_redirect_listener_definitions.php
+++ NEUE DATEIEN/includes/languages/german/extra_definitions/paypalr_redirect_listener_definitions.php
@@ -1,5 +1,7 @@
 <?php
 define('MODULE_PAYMENT_PAYPALR_REDIRECT_LISTENER_TRY_AGAIN', 'Bitte versuchen Sie es erneut oder wählen Sie eine andere Zahlungsart. Sollten weiterhin Schwierigkeiten auftreten, kontaktieren Sie uns bitte.');
 define('EMAIL_TEXT_ORDER_CUSTOMER_NEUTRAL', 'Guten Tag '); 
+define('EMAIL_TEXT_ORDER_CUSTOMER_GENDER_MALE', 'Sehr geehrter Herr ');
+define('EMAIL_TEXT_ORDER_CUSTOMER_GENDER_FEMALE', 'Sehr geehrte Frau ');
 define('OSH_EMAIL_TEXT_ORDER_CUSTOMER_NEUTRAL', 'Guten Tag '); 
 define('OSH_EMAIL_TEXT_UPDATEINFO', 'Wir informieren Sie über den Status Ihrer Bestellung bei '); 
